4a13ce522e55e63b8ef93bc38f885f4431bac792
jgonzales129 <jgonzales040302@gmail.com>
2026-05-16 14:28:23 +0800

    Update project.v

--- project.v
+++ project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_vga_example(
+module tt_um_leongamboa_OpenSilicon_SubmissionChapterLogo(
   input  wire [7:0] ui_in,    // Dedicated inputs
   output wire [7:0] uo_out,   // Dedicated outputs
   input  wire [7:0] uio_in,   // IOs: Input path
